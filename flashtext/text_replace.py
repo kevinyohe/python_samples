dee3688a0eb281fc1a5dd6c230d70f417352eec8
EC2 Default User <ec2-user@ip-172-31-25-159.us-east-2.compute.internal>
2019-03-18 16:37:10 +0000

    Code update

--- flashtext/text_replace.py
+++ flashtext/text_replace.py
@@ -1,2 +1,9 @@
-import flashtext
+from flashtext import KeywordProcessor
 
+string = 'hello from Jamaica! I need grapes'
+
+keyword_processor = KeywordProcessor()
+keyword_processor.add_keyword('Jamaica', 'J Town')
+
+new_sentence = keyword_processor.replace_keywords(string)
+print(new_sentence)
